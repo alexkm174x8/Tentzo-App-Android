60be136980afd2dd541f620d70e65c33abd4ae8b
Alejandro Kong <115722702+alexkm174x8@users.noreply.github.com>
2024-10-06 16:54:06 -0700

    Added map screen (missing map)

--- app/src/main/java/com/example/apptentzo_android/ui/Map/MapScreen.kt
+++ app/src/main/java/com/example/apptentzo_android/ui/Map/MapScreen.kt
@@ -1,2 +1,128 @@
-package com.example.apptentzo_android.ui.Map
+package com.example.apptentzo_android.ui.map
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+                MapScreen()
+        }
+    }
+}
+
+@Composable
+fun MapScreen() {
+    var boxHeight by remember { mutableStateOf(200.dp) }
+    val minHeight = 200.dp
+    val maxHeight = 700.dp
+
+    val density = LocalDensity.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+                .fillMaxWidth()
+                .height(boxHeight)
+                .background(Color.White)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        val dragInDp = with(density) { dragAmount.y.toDp() }
+                        boxHeight = (boxHeight - dragInDp).coerceIn(minHeight, maxHeight)
+                    }
+                }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Rutas",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(25.dp))
+                            .background(Color.Blue)
+                            .height(115.dp)
+                            .width(400.dp)
+                            .padding(10.dp)
+                            .clickable {
+                                //navController.navigate("detalle")
+                            }
+                    ) {
+                        Text(
+                            text = "Ruta 1",
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.align(Alignment.BottomStart)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, heightDp = 932, widthDp = 430)
+@Composable
+fun Map() {
+        MapScreen()
+}
