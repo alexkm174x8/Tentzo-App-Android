6f3cd112c92f8da39d1bc155b326d6109640ec18
Alejandro Kong <115722702+alexkm174x8@users.noreply.github.com>
2024-10-06 18:50:26 -0700

    Mixed files

--- app/src/main/java/com/example/apptentzo_android/ui/Camera/CameraScreen.kt
+++ app/src/main/java/com/example/apptentzo_android/ui/Camera/CameraScreen.kt
@@ -1,2 +1,82 @@
 package com.example.apptentzo_android.ui.Camera
 
+import android.Manifest
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CameraScreen()
+        }
+    }
+}
+
+@Composable
+fun CameraScreen() {
+    var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
+    val context = LocalContext.current
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(context, "Permiso concedido", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Permiso denegado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicturePreview()
+    ) { bitmap: Bitmap? ->
+        imageBitmap = bitmap
+    }
+
+    LaunchedEffect(Unit) {
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+    }
+
+    LaunchedEffect(imageBitmap) {
+        if (imageBitmap == null) {
+            cameraLauncher.launch(null)
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        imageBitmap?.let { bitmap ->
+            Image(
+                bitmap = bitmap.asImageBitmap(),
+                contentDescription = "Imagen capturada",
+                modifier = Modifier.size(200.dp)
+            )
+        } ?: run {
+            //mensaje si es que no se otorgan permisos
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CameraScreenPreview() {
+    CameraScreen()
+}
--- app/src/main/java/com/example/apptentzo_android/ui/Map/MapScreen.kt
+++ app/src/main/java/com/example/apptentzo_android/ui/Map/MapScreen.kt
@@ -1,82 +1,128 @@
-package com.example.experiment
+package com.example.apptentzo_android.ui.Map
 
-import android.Manifest
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            CameraScreen()
+                MapScreen()
         }
     }
 }
 
 @Composable
-fun CameraScreen() {
-    var imageBitmap by remember { mutableStateOf<Bitmap?>(null) }
-    val context = LocalContext.current
+fun MapScreen() {
+    var boxHeight by remember { mutableStateOf(200.dp) }
+    val minHeight = 200.dp
+    val maxHeight = 700.dp
 
-    val requestPermissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            Toast.makeText(context, "Permiso concedido", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(context, "Permiso denegado", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    val cameraLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.TakePicturePreview()
-    ) { bitmap: Bitmap? ->
-        imageBitmap = bitmap
-    }
-
-    LaunchedEffect(Unit) {
-        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
-    }
-
-    LaunchedEffect(imageBitmap) {
-        if (imageBitmap == null) {
-            cameraLauncher.launch(null)
-        }
-    }
+    val density = LocalDensity.current
 
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
     ) {
-        imageBitmap?.let { bitmap ->
-            Image(
-                bitmap = bitmap.asImageBitmap(),
-                contentDescription = "Imagen capturada",
-                modifier = Modifier.size(200.dp)
-            )
-        } ?: run {
-            //mensaje si es que no se otorgan permisos
+        Spacer(modifier = Modifier.weight(1f))
+
+        Box(
+            modifier = Modifier
+                .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+                .fillMaxWidth()
+                .height(boxHeight)
+                .background(Color.White)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        val dragInDp = with(density) { dragAmount.y.toDp() }
+                        boxHeight = (boxHeight - dragInDp).coerceIn(minHeight, maxHeight)
+                    }
+                }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Rutas",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(25.dp))
+                            .background(Color.Blue)
+                            .height(115.dp)
+                            .width(400.dp)
+                            .padding(10.dp)
+                            .clickable {
+                                //navController.navigate("detalle")
+                            }
+                    ) {
+                        Text(
+                            text = "Ruta 1",
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.align(Alignment.BottomStart)
+                        )
+                    }
+                }
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, heightDp = 932, widthDp = 430)
 @Composable
-fun CameraScreenPreview() {
-    CameraScreen()
+fun Map() {
+        MapScreen()
 }
